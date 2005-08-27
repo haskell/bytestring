793c3ed2b1e6ff51765d8ef28e780133c5b4f543
Don Stewart <dons@cse.unsw.edu.au>
2005-08-27 12:50:21 +0000

    Clean up properties

--- tests/Quick.hs
+++ tests/Quick.hs
@@ -11,8 +11,6 @@ import Test.QuickCheck
 -- at first we just check the correspondence to List functions
 
 prop_eq1 xs      = xs            == (unpackPS . packString $ xs) 
-prop_eq2 xs      = packString xs == packString xs
-prop_eq3 xs      = (packString . unpackPS . packString $ xs) == packString xs
 
 prop_compare1 xs  = (packString xs         `compare` packString xs) == EQ
 prop_compare2 xs  = (packString (xs++"X")  `compare` packString xs) == GT
@@ -26,41 +24,40 @@ prop_compare5 xs  = (not (null xs)) ==> (nilPS `compare` packString xs) == LT
 prop_cons1 xs = 'X' : xs == unpackPS ('X' `consPS` (packString xs))
 
 prop_head xs     = 
-    (not (null xs)) ==>
-        head xs  == headPS (packString xs)
+    (not (null xs)) ==> head xs  == (headPS . packString) xs
 
 prop_tail xs     = 
     (not (null xs)) ==>
-    packString (tail xs)    == tailPS (packString xs)
+    tail xs    == (unpackPS . tailPS . packString) xs
 
 prop_init xs     = 
     (not (null xs)) ==>
-    packString (init xs)    == initPS (packString xs)
+    init xs    == (unpackPS . initPS . packString) xs
 
 -- prop_null xs = (null xs) ==> null xs == (nullPS (packString xs))
 
 prop_length xs = length xs == lengthPS (packString xs)
 
-prop_append xs = packString (xs ++ xs) == packString xs `appendPS` packString xs
+prop_append xs = (xs ++ xs) == (unpackPS $ packString xs `appendPS` packString xs)
 
-prop_map   xs = packString (map toUpper xs) == mapPS toUpper (packString xs)
+prop_map   xs = map toLower xs == (unpackPS . (mapPS toLower) .  packString) xs
 
-prop_filter xs = packString (filter (=='X') xs) ==
-                 filterPS (=='X') (packString xs)
+prop_filter xs = (filter (=='X') xs) == (unpackPS $ filterPS (=='X') (packString xs))
 
-prop_foldl xs = (packString (foldl (\x c -> if c == 'a' then x else c:x) [] xs)) ==  
-                (foldlPS (\x c -> if c == 'a' then x else c `consPS` x) nilPS (packString xs))
+prop_foldl xs = ((foldl (\x c -> if c == 'a' then x else c:x) [] xs)) ==  
+                (unpackPS $ foldlPS (\x c -> if c == 'a' then x else c `consPS` x) nilPS (packString xs))
 
-prop_foldr xs = (packString (foldr (\c x -> if c == 'a' then x else c:x) [] xs)) ==  
-                (foldrPS (\c x -> if c == 'a' then x else c `consPS` x) nilPS (packString xs))
+prop_foldr xs = ((foldr (\c x -> if c == 'a' then x else c:x) [] xs)) ==  
+                (unpackPS $ foldrPS (\c x -> if c == 'a' then x else c `consPS` x) 
+                    nilPS (packString xs))
 
-prop_takeWhile xs = packString (takeWhile (/= 'X') xs) == takeWhilePS (/= 'X') (packString xs)
+prop_takeWhile xs = (takeWhile (/= 'X') xs) == (unpackPS . (takeWhilePS (/= 'X')) . packString) xs
 
-prop_dropWhile xs = packString (dropWhile (/= 'X') xs) == dropWhilePS (/= 'X') (packString xs)
+prop_dropWhile xs = (dropWhile (/= 'X') xs) == (unpackPS . (dropWhilePS (/= 'X')) . packString) xs
 
-prop_take xs = packString (take 10 xs) == takePS 10 (packString xs)
+prop_take xs = (take 10 xs) == (unpackPS . (takePS 10) . packString) xs
 
-prop_drop xs = packString (drop 10 xs) == dropPS 10 (packString xs)
+prop_drop xs = (drop 10 xs) == (unpackPS . (dropPS 10) . packString) xs
 
 prop_splitAt xs = (splitAt 1000 xs) == (let (x,y) = splitAtPS 1000 (packString xs) 
                                       in (unpackPS x, unpackPS y))
@@ -71,27 +68,27 @@ prop_span xs = (span (/='X') xs) == (let (x,y) = spanPS (/='X') (packString xs)
 prop_break xs = (break (/='X') xs) == (let (x,y) = breakPS (/='X') (packString xs) 
                                        in (unpackPS x, unpackPS y))
 
-prop_reverse xs = packString (reverse xs) == reversePS (packString xs)
+prop_reverse xs = (reverse xs) == (unpackPS . reversePS . packString) xs
 
 prop_elem xs = ('X' `elem` xs) == ('X' `elemPS` (packString xs))
 
 -- should try to stress it
-prop_concat1 xs = packString (concat [xs,xs]) == concatPS [packString xs, packString xs]
+prop_concat1 xs = (concat [xs,xs]) == (unpackPS $ concatPS [packString xs, packString xs])
 
-prop_concat2 xs = packString (concat [xs,[]]) == concatPS [packString xs, packString []]
+prop_concat2 xs = (concat [xs,[]]) == (unpackPS $ concatPS [packString xs, packString []])
 
 prop_any xs = (any (== 'X') xs) == (anyPS (== 'X') (packString xs))
 
-prop_lines xs = map packString (lines xs) == linesPS (packString xs)
+prop_lines xs = (lines xs) == ((map unpackPS) . linesPS . packString) xs
 
-prop_unlines xs = (packString.unlines.lines) xs == (unlinesPS . linesPS .packString) xs
+prop_unlines xs = (unlines.lines) xs == (unpackPS. unlinesPS . linesPS .packString) xs
 
-prop_words xs = map packString (words xs) == wordsPS (packString xs)
+prop_words xs = (words xs) == ((map unpackPS) . wordsPS . packString) xs
 
 prop_unwords xs = (packString.unwords.words) xs == (unwordsPS . wordsPS .packString) xs
 
-prop_join xs = (packString . concat . (intersperse "XYX") . lines) xs ==
-               (joinPS (packString "XYX") (linesPS (packString xs)))
+prop_join xs = (concat . (intersperse "XYX") . lines) xs ==
+               (unpackPS $ joinPS (packString "XYX") (linesPS (packString xs)))
 
 prop_elemIndex xs = (elemIndex 'X' xs) == (elemIndexPS 'X' (packString xs))
 
@@ -103,8 +100,6 @@ prop_findIndex xs = (fromMaybe (length xs) (findIndex (=='X') xs)) ==
 main = do
     runTests "fps" (defOpt { no_of_tests = 1000, length_of_tests= 10 } )
         [   run prop_eq1
-        ,   run prop_eq2
-        ,   run prop_eq3
         ,   run prop_compare1
         ,   run prop_compare2
         ,   run prop_compare3
