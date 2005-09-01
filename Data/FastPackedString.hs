6e7e4ae63ebcbf5242b0168adcd3d379763f1ff5
Don Stewart <dons@cse.unsw.edu.au>
2005-09-01 02:39:19 +0000

    Change license to BSD-style

--- Data/FastPackedString.hs
+++ Data/FastPackedString.hs
@@ -1,28 +1,13 @@
---
 -- Module      : FastPackedString
 -- Copyright   : (c) The University of Glasgow 2001,
 --               (c) David Roundy 2003-2005,
 --               (c) Don Stewart 2005
--- License     : GPL (Also happy to license this file as BSD style)
+-- License     : BSD-style
+--
 -- Maintainer  : dons@cse.unsw.edu.au
 -- Stability   : experimental
 -- Portability : portable
 -- 
--- This program is free software; you can redistribute it and\/or
--- modify it under the terms of the GNU General Public License as
--- published by the Free Software Foundation; either version 2 of
--- the License, or (at your option) any later version.
--- 
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
--- General Public License for more details.
--- 
--- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
--- 02111-1307, USA.
---
 
 --
 -- | A time and space-efficient implementation of strings as packed
@@ -35,7 +20,7 @@
 --
 -- Original GHC implementation by Bryan O\'Sullivan. Rewritten to use
 -- UArray by Simon Marlow. Rewritten to support slices and use
--- ForeignPtr by David Roundy. Cleaned up and extended by Don Stewart.
+-- ForeignPtr by David Roundy. Polished and extended by Don Stewart.
 --
 
 module Data.FastPackedString (
