7b6c50636d2c78ad7a27c15c6eec2be9d25caec4
Don Stewart <dons@cse.unsw.edu.au>
2006-04-21 01:18:24 +0000

    And guard the .h file , just for good measure

--- cbits/fpstring.h
+++ cbits/fpstring.h
@@ -1,6 +1,8 @@
 #include <HsFFI.h>
 
+#if defined(USE_MMAP)
 char *my_mmap(int len, int fd);
+#endif
 
 void reverse(unsigned char *dest, unsigned char *from, int len);
 void my_qsort(unsigned char *base, size_t size);
