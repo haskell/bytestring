e8c1c1429e9569b3a59830053f86af80afe42ce4
Don Stewart <dons@cse.unsw.edu.au>
2006-04-28 02:15:49 +0000

    Use Simon's qc framework. more amenable to testsuite error diffs

--- tests/Quick.hs
+++ tests/Quick.hs
@@ -4,13 +4,17 @@
 import Test.QuickCheck.Batch
 import Test.QuickCheck
 import Text.Show.Functions
-import System.Random
+
 import Data.List
 import Data.Char
 import Data.Word
 import Data.Maybe
+
 import Text.Printf
+
 import System.Environment
+import System.IO
+import System.Random
 
 import Data.ByteString.Char8 (ByteString, pack , unpack)
 import qualified Data.ByteString.Char8 as P
@@ -367,148 +371,191 @@ prop_unzip x = let (xs,ys) = unzip x in (pack xs, pack ys) == P.unzip x
 
 main = do
     x <- getArgs
-    let n = if null x then 200 else read . head $ x
-    do mapM_ (runTests "test" (defOpt { no_of_tests = n, length_of_tests = 10 })) $
-         breakUp 25 tests []
-
- where
-    breakUp _ [] acc = reverse acc
-    breakUp n xs acc = let h = take n xs
-                           r = drop n xs
-                       in breakUp n r (h:acc)
-
-    tests = [
-                 run prop_bijection
-            ,    run prop_bijection'
-            ,    run prop_eq1
-            ,    run prop_compare1
-            ,    run prop_compare2
-            ,    run prop_compare3
-            ,    run prop_compare4
-            ,    run prop_compare5
-            ,    run prop_compare6
-            ,    run prop_cons1
-            ,    run prop_cons2
-            ,    run prop_snoc1
-            ,    run prop_head
-            ,    run prop_head1
-            ,    run prop_tail
-            ,    run prop_tail1
-            ,    run prop_init
-            ,    run prop_length
-            ,    run prop_append1
-            ,    run prop_append2
-            ,    run prop_map
-            ,    run prop_filter1
-            ,    run prop_filter2
-            ,    run prop_foldl1
-            ,    run prop_foldl2
-            ,    run prop_foldr1
-            ,    run prop_foldr2
-            ,    run prop_all
-            ,    run prop_take
-            ,    run prop_drop
-            ,    run prop_takeWhile
-            ,    run prop_dropWhile
-            ,    run prop_splitAt
-            ,    run prop_span
-            ,    run prop_break
-            ,    run prop_reverse
-            ,    run prop_elem
-            ,    run prop_notElem
-            ,    run prop_concat1
-            ,    run prop_concat2
-            ,    run prop_any
-            ,    run prop_lines
-            ,    run prop_unlines
-            ,    run prop_words
-            ,    run prop_unwords
-            ,    run prop_join
-            ,    run prop_elemIndex1
-            ,    run prop_elemIndex2
-            ,    run prop_findIndex
-            ,    run prop_findIndicies
-            ,    run prop_elemIndices
-
-            ,    run prop_find
-            ,    run prop_sort1
-            ,    run prop_sort2
-            ,    run prop_sort3
-            ,    run prop_sort4
-            ,    run prop_sort5
-            ,    run prop_intersperse
-            ,    run prop_maximum
-            ,    run prop_minimum
-            ,    run prop_breakChar
-            ,    run prop_breakSpace
-            ,    run prop_dropSpace
-            ,    run prop_spanEnd
-            ,    run prop_breakFirst
-            ,    run prop_breakLast
-            ,    run prop_elemIndexLast1
-            ,    run prop_elemIndexLast2
-            ,    run prop_words'
-            ,    run prop_lines'
-            ,    run prop_dropSpaceEnd
-            ,    run prop_unfoldr
---          ,    run prop_addr
---          ,    run prop_addr2
-            ,    run prop_prefix
-            ,    run prop_suffix
-            ,    run prop_copy
-            ,    run prop_inits
-            ,    run prop_tails
-            ,    run prop_findSubstrings
-            ,    run prop_replicate1
-            ,    run prop_replicate2
-            ,    run prop_replicate3
-            ,    run prop_readint
-            ,    run prop_readint2
-            ,    run prop_filterChar1
-            ,    run prop_filterChar2
-            ,    run prop_filterChar3
-            ,    run prop_filterNotChar1
-            ,    run prop_filterNotChar2
-            ,    run prop_pack
-            ,    run prop_nil1
-            ,    run prop_nil2
-            ,    run prop_cons
-            ,    run prop_headS
-            ,    run prop_lengthS
-            ,    run prop_tailS
-            ,    run prop_null
-            ,    run prop_append
-            ,    run prop_index
-            ,    run prop_unsafeIndex
-            ,    run prop_mapS
-            ,    run prop_filter
-            ,    run prop_reverseS
-            ,    run prop_concat
-            ,    run prop_elemS
-    --      ,    run prop_substr
-            ,    run prop_takeS
-            ,    run prop_dropS
-            ,    run prop_splitAtS
-            ,    run prop_foldl
-            ,    run prop_foldr
-            ,    run prop_takeWhileS
-            ,    run prop_dropWhileS
-            ,    run prop_spanS
-            ,    run prop_breakS
-            ,    run prop_linesS
-            ,    run prop_unlinesS
-            ,    run prop_wordsS
-            ,    run prop_unwordsS
-            ,    run prop_wordstokens
-            ,    run prop_splitWith
-            ,    run prop_joinsplit
-            ,    run prop_lineIndices1
-            ,    run prop_count
-            ,    run prop_linessplit
-            ,    run prop_splitsplitWith
-            ,    run prop_joinjoinpath
-            ,    run prop_zip
-            ,    run prop_zip1
-            ,    run prop_zipWith
-            ,    run prop_unzip
+    let n = if null x then 100 else read . head $ x
+    mapM_ (\(s,a) -> printf "%-15s: " s >> a n) tests
+  where
+    tests = [    ("bijection",       mytest prop_bijection)
+            ,    ("bijection'",       mytest prop_bijection')
+            ,    ("eq1",       mytest prop_eq1)
+            ,    ("compare1",       mytest prop_compare1)
+            ,    ("compare2",       mytest prop_compare2)
+            ,    ("compare3",       mytest prop_compare3)
+            ,    ("compare4",       mytest prop_compare4)
+            ,    ("compare5",       mytest prop_compare5)
+            ,    ("compare6",       mytest prop_compare6)
+            ,    ("cons1",       mytest prop_cons1)
+            ,    ("cons2",       mytest prop_cons2)
+            ,    ("snoc1",       mytest prop_snoc1)
+            ,    ("head",       mytest prop_head)
+            ,    ("head1",       mytest prop_head1)
+            ,    ("tail",       mytest prop_tail)
+            ,    ("tail1",       mytest prop_tail1)
+            ,    ("init",       mytest prop_init)
+            ,    ("length",       mytest prop_length)
+            ,    ("append1",       mytest prop_append1)
+            ,    ("append2",       mytest prop_append2)
+            ,    ("map",       mytest prop_map)
+            ,    ("filter1",       mytest prop_filter1)
+            ,    ("filter2",       mytest prop_filter2)
+            ,    ("foldl1",       mytest prop_foldl1)
+            ,    ("foldl2",       mytest prop_foldl2)
+            ,    ("foldr1",       mytest prop_foldr1)
+            ,    ("foldr2",       mytest prop_foldr2)
+            ,    ("all",       mytest prop_all)
+            ,    ("take",       mytest prop_take)
+            ,    ("drop",       mytest prop_drop)
+            ,    ("takeWhile",       mytest prop_takeWhile)
+            ,    ("dropWhile",       mytest prop_dropWhile)
+            ,    ("splitAt",       mytest prop_splitAt)
+            ,    ("span",       mytest prop_span)
+            ,    ("break",       mytest prop_break)
+            ,    ("reverse",       mytest prop_reverse)
+            ,    ("elem",       mytest prop_elem)
+            ,    ("notElem",       mytest prop_notElem)
+            ,    ("concat1",       mytest prop_concat1)
+            ,    ("concat2",       mytest prop_concat2)
+            ,    ("any",       mytest prop_any)
+            ,    ("lines",       mytest prop_lines)
+            ,    ("unlines",       mytest prop_unlines)
+            ,    ("words",       mytest prop_words)
+            ,    ("unwords",       mytest prop_unwords)
+            ,    ("join",       mytest prop_join)
+            ,    ("elemIndex1",       mytest prop_elemIndex1)
+            ,    ("elemIndex2",       mytest prop_elemIndex2)
+            ,    ("findIndex",       mytest prop_findIndex)
+            ,    ("findIndicies",       mytest prop_findIndicies)
+            ,    ("elemIndices",       mytest prop_elemIndices)
+            ,    ("find",       mytest prop_find)
+            ,    ("sort1",       mytest prop_sort1)
+            ,    ("sort2",       mytest prop_sort2)
+            ,    ("sort3",       mytest prop_sort3)
+            ,    ("sort4",       mytest prop_sort4)
+            ,    ("sort5",       mytest prop_sort5)
+            ,    ("intersperse",       mytest prop_intersperse)
+            ,    ("maximum",       mytest prop_maximum)
+            ,    ("minimum",       mytest prop_minimum)
+            ,    ("breakChar",       mytest prop_breakChar)
+            ,    ("breakSpace",       mytest prop_breakSpace)
+            ,    ("dropSpace",       mytest prop_dropSpace)
+            ,    ("spanEnd",       mytest prop_spanEnd)
+            ,    ("breakFirst",       mytest prop_breakFirst)
+            ,    ("breakLast",       mytest prop_breakLast)
+            ,    ("elemIndexLast1",       mytest prop_elemIndexLast1)
+            ,    ("elemIndexLast2",       mytest prop_elemIndexLast2)
+            ,    ("words'",       mytest prop_words')
+            ,    ("lines'",       mytest prop_lines')
+            ,    ("dropSpaceEnd",       mytest prop_dropSpaceEnd)
+            ,    ("unfoldr",       mytest prop_unfoldr)
+            ,    ("prefix",       mytest prop_prefix)
+            ,    ("suffix",       mytest prop_suffix)
+            ,    ("copy",       mytest prop_copy)
+            ,    ("inits",       mytest prop_inits)
+            ,    ("tails",       mytest prop_tails)
+            ,    ("findSubstrings",       mytest prop_findSubstrings)
+            ,    ("replicate1",       mytest prop_replicate1)
+            ,    ("replicate2",       mytest prop_replicate2)
+            ,    ("replicate3",       mytest prop_replicate3)
+            ,    ("readint",       mytest prop_readint)
+            ,    ("readint2",       mytest prop_readint2)
+            ,    ("filterChar1",       mytest prop_filterChar1)
+            ,    ("filterChar2",       mytest prop_filterChar2)
+            ,    ("filterChar3",       mytest prop_filterChar3)
+            ,    ("filterNotChar1",       mytest prop_filterNotChar1)
+            ,    ("filterNotChar2",       mytest prop_filterNotChar2)
+            ,    ("pack",       mytest prop_pack)
+            ,    ("nil1",       mytest prop_nil1)
+            ,    ("nil2",       mytest prop_nil2)
+            ,    ("cons",       mytest prop_cons)
+            ,    ("headS",       mytest prop_headS)
+            ,    ("lengthS",       mytest prop_lengthS)
+            ,    ("tailS",       mytest prop_tailS)
+            ,    ("null",       mytest prop_null)
+            ,    ("append",       mytest prop_append)
+            ,    ("index",       mytest prop_index)
+            ,    ("unsafeIndex",       mytest prop_unsafeIndex)
+            ,    ("mapS",       mytest prop_mapS)
+            ,    ("filter",       mytest prop_filter)
+            ,    ("reverseS",       mytest prop_reverseS)
+            ,    ("concat",       mytest prop_concat)
+            ,    ("elemS",       mytest prop_elemS)
+            ,    ("takeS",       mytest prop_takeS)
+            ,    ("dropS",       mytest prop_dropS)
+            ,    ("splitAtS",       mytest prop_splitAtS)
+            ,    ("foldl",       mytest prop_foldl)
+            ,    ("foldr",       mytest prop_foldr)
+            ,    ("takeWhileS",       mytest prop_takeWhileS)
+            ,    ("dropWhileS",       mytest prop_dropWhileS)
+            ,    ("spanS",       mytest prop_spanS)
+            ,    ("breakS",       mytest prop_breakS)
+            ,    ("linesS",       mytest prop_linesS)
+            ,    ("unlinesS",       mytest prop_unlinesS)
+            ,    ("wordsS",       mytest prop_wordsS)
+            ,    ("unwordsS",       mytest prop_unwordsS)
+            ,    ("wordstokens",       mytest prop_wordstokens)
+            ,    ("splitWith",       mytest prop_splitWith)
+            ,    ("joinsplit",       mytest prop_joinsplit)
+            ,    ("lineIndices1",       mytest prop_lineIndices1)
+            ,    ("count",       mytest prop_count)
+            ,    ("linessplit",       mytest prop_linessplit)
+            ,    ("splitsplitWith",       mytest prop_splitsplitWith)
+            ,    ("joinjoinpath",       mytest prop_joinjoinpath)
+            ,    ("zip",       mytest prop_zip)
+            ,    ("zip1",       mytest prop_zip1)
+            ,    ("zipWith",       mytest prop_zipWith)
+            ,    ("unzip",       mytest prop_unzip)
             ]
+
+mytest :: Testable a => a -> Int -> IO ()
+mytest a n = mycheck defaultConfig{configMaxTest=n} a
+
+mycheck :: Testable a => Config -> a -> IO ()
+mycheck config a =
+  do let rnd = mkStdGen 99
+     mytests config (evaluate a) rnd 0 0 []
+
+mytests :: Config -> Gen Result -> StdGen -> Int -> Int -> [[String]] -> IO ()
+mytests config gen rnd0 ntest nfail stamps
+  | ntest == configMaxTest config = do done "OK," ntest stamps
+  | nfail == configMaxFail config = do done "Arguments exhausted after" ntest stamps
+  | otherwise               =
+      do putStr (configEvery config ntest (arguments result)) >> hFlush stdout
+         case ok result of
+           Nothing    ->
+             mytests config gen rnd1 ntest (nfail+1) stamps
+           Just True  ->
+             mytests config gen rnd1 (ntest+1) nfail (stamp result:stamps)
+           Just False ->
+             putStr ( "Falsifiable after "
+                   ++ show ntest
+                   ++ " tests:\n"
+                   ++ unlines (arguments result)
+                    ) >> hFlush stdout
+     where
+      result      = generate (configSize config ntest) rnd2 gen
+      (rnd1,rnd2) = split rnd0
+
+done :: String -> Int -> [[String]] -> IO ()
+done mesg ntest stamps =
+  do putStr ( mesg ++ " " ++ show ntest ++ " tests" ++ table )
+ where
+  table = display
+        . map entry
+        . reverse
+        . sort
+        . map pairLength
+        . group
+        . sort
+        . filter (not . null)
+        $ stamps
+
+  display []  = ".\n"
+  display [x] = " (" ++ x ++ ").\n"
+  display xs  = ".\n" ++ unlines (map (++ ".") xs)
+
+  pairLength xss@(xs:_) = (length xss, xs)
+  entry (n, xs)         = percentage n ntest
+                       ++ " "
+                       ++ concat (intersperse ", " xs)
+
+  percentage n m        = show ((100 * n) `div` m) ++ "%"
+
