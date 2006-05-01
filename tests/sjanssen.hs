984855449d72ef0886f674fa7f5af0db71d605e6
Don Stewart <dons@cse.unsw.edu.au>
2006-05-01 03:01:02 +0000

    Bit faster if we filter before we map

--- tests/sjanssen.hs
+++ tests/sjanssen.hs
@@ -5,9 +5,9 @@ import qualified Data.ByteString.Char8 as P
 main =
     mapM (\(a, b) -> putStrLn ([a] ++ ": " ++ show b))
     . sortBy (\a b -> snd b `compare` snd a)
-    . map (\x -> (P.head x, P.length x))
+    . map (\x -> (P.unsafeHead x, P.length x))
     . P.group
     . P.sort
-    . P.filter isAlpha
     . P.map toLower
+    . P.filter isAlpha
     =<< P.getContents
