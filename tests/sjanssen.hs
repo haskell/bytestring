c30df5700356b37856a25ef3da47422f7b2c7b13
Don Stewart <dons@cse.unsw.edu.au>
2006-05-01 02:53:39 +0000

    Another benchmark

---
+++ tests/sjanssen.hs
@@ -0,0 +1,13 @@
+import Data.Char (isAlpha, toLower)
+import Data.List (sortBy)
+import qualified Data.ByteString.Char8 as P
+
+main =
+    mapM (\(a, b) -> putStrLn ([a] ++ ": " ++ show b))
+    . sortBy (\a b -> snd b `compare` snd a)
+    . map (\x -> (P.head x, P.length x))
+    . P.group
+    . P.sort
+    . P.filter isAlpha
+    . P.map toLower
+    =<< P.getContents
