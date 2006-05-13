6ff594099b9f78debaeef32474f9d6533390e206
Don Stewart <dons@cse.unsw.edu.au>
2006-05-13 03:07:58 +0000

    More useful lazyio.hs

--- tests/lazyio.hs
+++ tests/lazyio.hs
@@ -1,6 +1,9 @@
 import qualified Data.ByteString.Lazy as L
 import System.IO
+import System.Environment
 
-main = L.hGetContentsN (64*1024) stdin >>= L.hPut stdout .  L.filterNotByte 101
+main = do
+    n <- getArgs >>= readIO . head
+    L.hGetContentsN (n*1024) stdin >>= L.hPut stdout .  L.filterNotByte 101
 
 -- main = L.hGet stdin 10000000000 >>= print . L.length
