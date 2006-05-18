6a7c420f2f735a58367dab805c741e0e3c9f63a0
Don Stewart <dons@cse.unsw.edu.au>
2006-05-18 05:59:49 +0000

    Use readFile, it'll avoid realloc issues

--- tests/wc.hs
+++ tests/wc.hs
@@ -1,6 +1,8 @@
 import qualified Data.ByteString.Char8 as B
+main = print . length . B.lines =<< B.readFile "bigdata" -- B.getContents
 
-main = print . length . B.lines =<< B.getContents
+-- import qualified Data.ByteString.Lazy as L
+-- main = print . L.count 10 =<< L.getContents
 
 --
 -- rule should rewrite this to:
