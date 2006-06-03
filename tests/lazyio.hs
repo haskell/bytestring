ef4142c983900525bdabe6943ad1c35c1f906151
Don Stewart <dons@cse.unsw.edu.au>
2006-06-03 07:21:06 +0000

    tweak

--- tests/lazyio.hs
+++ tests/lazyio.hs
@@ -4,6 +4,6 @@ import System.Environment
 
 main = do
     n <- getArgs >>= readIO . head
-    L.hGetContentsN (n*1024) stdin >>= L.hPut stdout .  L.filterNotByte 101
+    L.hGetContentsN n stdin >>= L.hPut stdout .  L.filterNotByte 101
 
 -- main = L.hGet stdin 10000000000 >>= print . L.length
