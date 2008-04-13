b7bc41b724a881fd097e48791b07968c7418626c
Don Stewart <dons@galois.com>
2008-04-13 08:59:55 +0000

    return failures

--- tests/QuickCheckUtils.hs
+++ tests/QuickCheckUtils.hs
@@ -55,16 +55,17 @@ instance Functor (Either a) where
 -- Enable this to get verbose test output. Including the actual tests.
 debug = False
 
-mytest :: Testable a => a -> Int -> IO ()
+mytest :: Testable a => a -> Int -> IO (Bool, Int)
 mytest a n = mycheck defaultConfig
     { configMaxTest=n
     , configEvery= \n args -> if debug then show n ++ ":\n" ++ unlines args else [] } a
 
-mycheck :: Testable a => Config -> a -> IO ()
+mycheck :: Testable a => Config -> a -> IO (Bool, Int)
 mycheck config a =
   do rnd <- newStdGen
      mytests config (evaluate a) rnd 0 0 []
 
+{-
 mytests :: Config -> Gen Result -> StdGen -> Int -> Int -> [[String]] -> IO ()
 mytests config gen rnd0 ntest nfail stamps
   | ntest == configMaxTest config = do done "OK," ntest stamps
@@ -82,6 +83,28 @@ mytests config gen rnd0 ntest nfail stamps
                    ++ " tests:\n"
                    ++ unlines (arguments result)
                     ) >> hFlush stdout
+     where
+      result      = generate (configSize config ntest) rnd2 gen
+      (rnd1,rnd2) = split rnd0
+-}
+
+mytests :: Config -> Gen Result -> StdGen -> Int -> Int -> [[String]] -> IO (Bool, Int)
+mytests config gen rnd0 ntest nfail stamps
+    | ntest == configMaxTest config = done "OK," ntest stamps >> return (True, ntest)
+    | nfail == configMaxFail config = done "Arguments exhausted after" ntest stamps >> return (True, ntest)
+    | otherwise               =
+      do putStr (configEvery config ntest (arguments result)) >> hFlush stdout
+         case ok result of
+           Nothing    ->
+             mytests config gen rnd1 ntest (nfail+1) stamps
+           Just True  ->
+             mytests config gen rnd1 (ntest+1) nfail (stamp result:stamps)
+           Just False ->
+             putStr ( "Falsifiable after "
+                   ++ show ntest
+                   ++ " tests:\n"
+                   ++ unlines (arguments result)
+                    ) >> hFlush stdout >> return (False, ntest)
      where
       result      = generate (configSize config ntest) rnd2 gen
       (rnd1,rnd2) = split rnd0
