05ebb50161c29e85274f6db22f57ab597dc044ab
Duncan Coutts <duncan@haskell.org>
2010-07-30 12:24:09 +0000

    Fix for SPJs new typechecking system A local value was being generalised to Ptr a, and used at type Ptr Word8 and also at type Ptr CChar. SPJs new type checker prefers not to generalise local bindings such as this. This instance is also a bit wierd from an engineering point of view. The code is now more explicit that we are using a pointer at two different types, by using a castPtr, which is the same as the way we handle other CString cases.

--- Data/ByteString/Unsafe.hs
+++ Data/ByteString/Unsafe.hs
@@ -153,10 +153,11 @@ unsafeDrop n (PS x s l) = assert (0 <= n && n <= l) $ PS x (s+n) (l-n)
 --
 unsafePackAddress :: Addr# -> IO ByteString
 unsafePackAddress addr# = do
-    p <- newForeignPtr_ cstr
+    p <- newForeignPtr_ (castPtr cstr)
     l <- c_strlen cstr
     return $ PS p 0 (fromIntegral l)
   where
+    cstr :: CString
     cstr = Ptr addr#
 {-# INLINE unsafePackAddress #-}
 
