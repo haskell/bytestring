facbc535fcac0dc4a1ddf29e638c1b84af00ae02
Karl D. Asmussen <mhd@herebedragons.dk>
2014-10-30 08:59:51 +0100

    Added breakByteEnd to mirror breakByte

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -689,6 +689,7 @@ prop_breakspan xs c = L.break (==c) xs == L.span (/=c) xs
 prop_span xs a = (span (/=a) xs) == (let (x,y) = L.span (/=a) (pack xs) in (unpack x, unpack y))
 
 -- prop_breakByte xs c = L.break (== c) xs == L.breakByte c xs
+-- prop_breakByteEnd xs c = L.breakEnd (== c) xs == L.breakByteEnd c xs
 
 -- prop_spanByte c xs = (L.span (==c) xs) == L.spanByte c xs
 
@@ -1463,6 +1464,8 @@ prop_length_loop_fusion_4 f1 acc1 xs =
 
 -- prop_break_spec x s =
 --     P.break ((==) x) s == P.breakByte x s
+-- prop_breakEnd_spec x s =
+--     P.breakEnd ((==) x) s == P.breakByteEnd x s
 
 -- prop_span_spec x s =
 --     P.span ((==) x) s == P.spanByte x s
@@ -2412,6 +2415,7 @@ bb_tests =
 --  , testProperty "join/joinByte"  prop_join_spec
 --  , testProperty "span/spanByte"  prop_span_spec
 --  , testProperty "break/breakByte"prop_break_spec
+--  , testProperty "break/breakByteEnd"prop_breakEnd_spec
     ]
 
 ------------------------------------------------------------------------
@@ -2540,6 +2544,7 @@ ll_tests =
     , testProperty "splitAt"            prop_splitAt
     , testProperty "break/span"         prop_breakspan
 --  , testProperty "break/breakByte"    prop_breakByte
+--  , testProperty "break/breakByteEnd" prop_breakByteEnd
 --  , testProperty "span/spanByte"      prop_spanByte
     , testProperty "split"              prop_split
     , testProperty "splitWith"          prop_splitWith
