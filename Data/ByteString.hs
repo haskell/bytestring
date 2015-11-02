de7331e232c7502629a18f44a44c1f909ccccc39
Sean <burton.seanr@gmail.com>
2015-11-02 15:58:01 +0000

    Fix build breakage.

--- Data/ByteString.hs
+++ Data/ByteString.hs
@@ -279,6 +279,11 @@ import GHC.Prim                 (Word#)
 import GHC.Base                 (build)
 import GHC.Word hiding (Word8)
 
+#if !(MIN_VERSION_BASE(4,7,0))
+finiteBitSize :: Bits a => a -> Int
+finiteBitSize = bitSize
+#endif
+
 -- -----------------------------------------------------------------------------
 -- Introducing and eliminating 'ByteString's
 
