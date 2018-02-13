560c101139072e68ef1b9c0e4d618032d227227d
Sergey Vinokurov <serg.foo@gmail.com>
2018-02-13 22:39:36 +0000

    Remove haddock comment for a function in a where-block

--- Data/ByteString.hs
+++ Data/ByteString.hs
@@ -1521,9 +1521,8 @@ sort (PS input s l) = unsafeCreate l $ \p -> allocaArray 256 $ \arr -> do
                          go (i + 1) (ptr `plusPtr` fromIntegral n)
     go 0 p
   where
-    -- | Count the number of occurrences of each byte.
+    -- Count the number of occurrences of each byte.
     -- Used by 'sort'
-    --
     countOccurrences :: Ptr CSize -> Ptr Word8 -> Int -> IO ()
     countOccurrences !counts !str !len = go 0
      where
