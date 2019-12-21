46affe489feccd61555907080ece20bcbfc7b58f
Sebastian Nagel <sebastian.nagel@ncoding.at>
2019-12-21 21:13:42 +0100

    Explitly import S# data constructor

--- Data/ByteString/Builder/ASCII.hs
+++ Data/ByteString/Builder/ASCII.hs
@@ -105,7 +105,7 @@ import           GHC.Types   (Int(..))
 # if __GLASGOW_HASKELL__ < 611
 import GHC.Integer.Internals
 # else
-import GHC.Integer.GMP.Internals -- for S# constructor
+import GHC.Integer.GMP.Internals (Integer(S#))
 # endif
 #endif
 
