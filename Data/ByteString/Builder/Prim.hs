e687b9d094a1839d060a2e5f2640670855a47d70
Viktor Dukhovni <viktor1ghub@dukhovni.org>
2020-06-24 20:38:47 -0200

    Fix nits in primMapByteStringBounded haddock. (#225)
    - Primas -> Prim as
    - Need to lift FixedPrim word8 -> BoundedPrim for use with condB
    
    Co-authored-by: Viktor Dukhovni <ietf-dane@dukhovni.org>

--- Data/ByteString/Builder/Prim.hs
+++ Data/ByteString/Builder/Prim.hs
@@ -633,9 +633,9 @@ primUnfoldrBounded w f x0 =
 -- using a 'BoundedPrim'. For example, we can write a 'Builder' that filters
 -- a strict 'S.ByteString' as follows.
 --
--- > import Data.ByteString.Builder.Primas P (word8, condB, emptyB)
+-- > import qualified Data.ByteString.Builder.Prim as P
 --
--- > filterBS p = P.condB p P.word8 P.emptyB
+-- > filterBS p = P.condB p (P.liftFixedToBounded P.word8) P.emptyB
 --
 {-# INLINE primMapByteStringBounded #-}
 primMapByteStringBounded :: BoundedPrim Word8 -> S.ByteString -> Builder
