fe9b12caa8b1c3b4c813a1b846ad8326552c5881
Bodigrim <andrew.lelechenko@gmail.com>
2021-12-06 21:04:44 +0000

    Further deduplication

--- cbits/is-valid-utf8.c
+++ cbits/is-valid-utf8.c
@@ -52,12 +52,10 @@ SUCH DAMAGE.
 #define to_little_endian(x) (x)
 #endif
 
-#ifdef __x86_64__
+// 0x80 in every 'lane'.
+static uint64_t const high_bits_mask = 0x8080808080808080ULL;
 
-// Fallback (for tails)
-
-static inline int is_valid_utf8_fallback(uint8_t const *const src,
-                                         size_t const len) {
+static inline int is_valid_utf8_fallback(uint8_t const *const src, size_t const len) {
   uint8_t const *ptr = (uint8_t const *)src;
   // This is 'one past the end' to make loop termination and bounds checks
   // easier.
@@ -67,6 +65,40 @@ static inline int is_valid_utf8_fallback(uint8_t const *const src,
     // Check if the byte is ASCII.
     if (byte <= 0x7F) {
       ptr++;
+      // If we saw one ASCII byte, as long as it's not whitespace, it's quite
+      // likely we'll see more.
+      bool is_not_whitespace = byte > 32;
+      // If possible, do a block-check ahead.
+      if ((ptr + 32 < end) && is_not_whitespace) {
+        uint64_t const *big_ptr = (uint64_t const *)ptr;
+        // Non-ASCII bytes have a set MSB. Thus, if we AND with 0x80 in every
+        // 'lane', we will get 0 if everything is ASCII, and something else
+        // otherwise.
+        uint64_t results[4] = {to_little_endian(*big_ptr) & high_bits_mask,
+                               to_little_endian(*(big_ptr + 1)) & high_bits_mask,
+                               to_little_endian(*(big_ptr + 2)) & high_bits_mask,
+                               to_little_endian(*(big_ptr + 3)) & high_bits_mask};
+        if (results[0] == 0) {
+          ptr += 8;
+          if (results[1] == 0) {
+            ptr += 8;
+            if (results[2] == 0) {
+              ptr += 8;
+              if (results[3] == 0) {
+                ptr += 8;
+              } else {
+                ptr += (__builtin_ctzl(results[3]) / 8);
+              }
+            } else {
+              ptr += (__builtin_ctzl(results[2]) / 8);
+            }
+          } else {
+            ptr += (__builtin_ctzl(results[1]) / 8);
+          }
+        } else {
+          ptr += (__builtin_ctzl(results[0]) / 8);
+        }
+      }
     }
     // Check for a valid 2-byte sequence.
     //
@@ -121,6 +153,8 @@ static inline int is_valid_utf8_fallback(uint8_t const *const src,
   // If we got this far, we're valid.
   return 1;
 }
+
+#ifdef __x86_64__
 
 // SSE2
 
@@ -617,108 +651,6 @@ is_valid_utf8_avx2(uint8_t const *const src, size_t const len) {
 
 #endif
 
-// 0x80 in every 'lane'.
-static uint64_t const high_bits_mask = 0x8080808080808080ULL;
-
-int bytestring_is_valid_utf8_fallback(uint8_t const *const src, size_t const len) {
-  uint8_t const *ptr = (uint8_t const *)src;
-  // This is 'one past the end' to make loop termination and bounds checks
-  // easier.
-  uint8_t const *const end = ptr + len;
-  while (ptr < end) {
-    uint8_t const byte = *ptr;
-    // Check if the byte is ASCII.
-    if (byte <= 0x7F) {
-      ptr++;
-      // If we saw one ASCII byte, as long as it's not whitespace, it's quite
-      // likely we'll see more.
-      bool is_not_whitespace = byte > 32;
-      // If possible, do a block-check ahead.
-      if ((ptr + 32 < end) && is_not_whitespace) {
-        uint64_t const *big_ptr = (uint64_t const *)ptr;
-        // Non-ASCII bytes have a set MSB. Thus, if we AND with 0x80 in every
-        // 'lane', we will get 0 if everything is ASCII, and something else
-        // otherwise.
-        uint64_t results[4] = {to_little_endian(*big_ptr) & high_bits_mask,
-                               to_little_endian(*(big_ptr + 1)) & high_bits_mask,
-                               to_little_endian(*(big_ptr + 2)) & high_bits_mask,
-                               to_little_endian(*(big_ptr + 3)) & high_bits_mask};
-        if (results[0] == 0) {
-          ptr += 8;
-          if (results[1] == 0) {
-            ptr += 8;
-            if (results[2] == 0) {
-              ptr += 8;
-              if (results[3] == 0) {
-                ptr += 8;
-              } else {
-                ptr += (__builtin_ctzl(results[3]) / 8);
-              }
-            } else {
-              ptr += (__builtin_ctzl(results[2]) / 8);
-            }
-          } else {
-            ptr += (__builtin_ctzl(results[1]) / 8);
-          }
-        } else {
-          ptr += (__builtin_ctzl(results[0]) / 8);
-        }
-      }
-    }
-    // Check for a valid 2-byte sequence.
-    //
-    // We use a signed comparison to avoid an extra comparison with 0x80, since
-    // _signed_ 0x80 is -128.
-    else if (ptr + 1 < end && byte >= 0xC2 && byte <= 0xDF &&
-             ((int8_t) * (ptr + 1)) <= (int8_t)0xBF) {
-      ptr += 2;
-    }
-    // Check for a valid 3-byte sequence.
-    else if (ptr + 2 < end && byte >= 0xE0 && byte <= 0xEF) {
-      uint8_t const byte2 = *(ptr + 1);
-      bool byte2_valid = (int8_t)byte2 <= (int8_t)0xBF;
-      bool byte3_valid = ((int8_t) * (ptr + 2)) <= (int8_t)0xBF;
-      if (byte2_valid && byte3_valid &&
-          // E0, A0..BF, 80..BF
-          ((byte == 0xE0 && byte2 >= 0xA0) ||
-           // E1..EC, 80..BF, 80..BF
-           (byte >= 0xE1 && byte <= 0xEC) ||
-           // ED, 80..9F, 80..BF
-           (byte == 0xED && byte2 <= 0x9F) ||
-           // EE..EF, 80..BF, 80..BF
-           (byte >= 0xEE && byte <= 0xEF))) {
-        ptr += 3;
-      } else {
-        return 0;
-      }
-    }
-    // Check for a valid 4-byte sequence.
-    else if (ptr + 3 < end) {
-      uint8_t const byte2 = *(ptr + 1);
-      bool byte2_valid = (int8_t)byte2 <= (int8_t)0xBF;
-      bool byte3_valid = ((int8_t) * (ptr + 2)) <= (int8_t)0xBF;
-      bool byte4_valid = ((int8_t) * (ptr + 3)) <= (int8_t)0xBF;
-      if (byte2_valid && byte3_valid && byte4_valid &&
-          // F0, 90..BF, 80..BF, 80..BF
-          ((byte == 0xF0 && byte2 >= 0x90) ||
-           // F1..F3, 80..BF, 80..BF, 80..BF
-           (byte >= 0xF1 && byte <= 0xF3) ||
-           // F4, 80..8F, 80..BF, 80..BF
-           (byte == 0xF4 && byte2 <= 0x8F))) {
-        ptr += 4;
-      } else {
-        return 0;
-      }
-    }
-    // Otherwise, invalid.
-    else {
-      return 0;
-    }
-  }
-  // If we got this far, we're valid.
-  return 1;
-}
-
 #ifdef __x86_64__
 bool has_sse2() {
   uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
@@ -752,12 +684,12 @@ int bytestring_is_valid_utf8(uint8_t const *const src, size_t const len) {
   static _Atomic is_valid_utf8_t s_impl = (is_valid_utf8_t)NULL;
   is_valid_utf8_t impl = atomic_load_explicit(&s_impl, memory_order_relaxed);
   if (!impl) {
-    impl = has_avx2() ? is_valid_utf8_avx2 : (has_ssse3() ? is_valid_utf8_ssse3 : (has_sse2() ? is_valid_utf8_sse2 : bytestring_is_valid_utf8_fallback));
+    impl = has_avx2() ? is_valid_utf8_avx2 : (has_ssse3() ? is_valid_utf8_ssse3 : (has_sse2() ? is_valid_utf8_sse2 : is_valid_utf8_fallback));
     atomic_store_explicit(&s_impl, impl, memory_order_relaxed);
   }
   return (*impl)(src, len);
 #else
-  return bytestring_is_valid_utf8_fallback(src, len);
+  return is_valid_utf8_fallback(src, len);
 #endif
 }
 
