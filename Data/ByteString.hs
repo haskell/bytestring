39fcb564d1fd33a862e07412512ee6d131224039
dan soucy <dev@danso.ca>
2022-04-14 12:41:53 -0400

    remove reference to 'sublist' from group doc

--- Data/ByteString.hs
+++ Data/ByteString.hs
@@ -1176,7 +1176,7 @@ split w (BS x l) = loop 0
 
 -- | The 'group' function takes a ByteString and returns a list of
 -- ByteStrings such that the concatenation of the result is equal to the
--- argument.  Moreover, each sublist in the result contains only equal
+-- argument.  Moreover, each string in the result contains only equal
 -- elements.  For example,
 --
 -- > group "Mississippi" = ["M","i","ss","i","ss","i","pp","i"]
