21d46be08230b528a5abbff6e1155c7aed843abb
Matthew Craven <clyring@gmail.com>
2023-05-21 22:06:42 -0400

    fix new old-ghc stuff

--- Data/ByteString/Builder/Prim/Internal/Floating.hs
+++ Data/ByteString/Builder/Prim/Internal/Floating.hs
@@ -26,6 +26,7 @@ import GHC.Float (castFloatToWord32, castDoubleToWord64)
 #else
 import Foreign.Marshal.Utils
 import Foreign.Storable
+import Foreign.Ptr
 {-
 We work around ticket http://ghc.haskell.org/trac/ghc/ticket/4092 by
 storing the Float/Double in a temp buffer and peeking it out again from there.
@@ -50,7 +51,7 @@ encodeDoubleViaWord64F :: FixedPrim Word64 -> FixedPrim Double
 #if HS_CAST_FLOAT_WORD_OPS_AVAILABLE
 encodeDoubleViaWord64F = (castDoubleToWord64 >$<)
 #else
-encodeDoubleViaWord64F = fixedPrim (size w64fe) $ \x op -> do
+encodeDoubleViaWord64F w64fe = fixedPrim (size w64fe) $ \x op -> do
   x' <- with x (peek . castPtr)
   runF w64fe x' op
 #endif
