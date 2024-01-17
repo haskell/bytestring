f3b9b0eb9c213c0b17fc5d15b6ffcb0100e2fff9
Matthew Craven <clyring@gmail.com>
2024-01-16 22:39:39 -0500

    Use primops for unaligned writes when possible

--- include/bytestring-cpp-macros.h
+++ include/bytestring-cpp-macros.h
@@ -34,3 +34,8 @@ These operations were added in base-4.10.0, but due to
 https://gitlab.haskell.org/ghc/ghc/-/issues/16617 they
 are buggy with negative floats before ghc-8.10.
 */
+
+#define HS_UNALIGNED_ADDR_PRIMOPS_AVAILABLE __GLASGOW_HASKELL__ >= 909
+/*
+This should really be MIN_VERSION_base(4,20,0).
+*/
